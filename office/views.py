d78967ccbf41a8efe61c089f4efa8b23d134f8a3
cesar rodriguez <streamlink777@live.com>
2019-06-26 11:53:30 -0400

    removal of drawings Tracking

--- office/views.py
+++ office/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Forman_hub import FuntionRoot
 
 
 def main_view(request):
+
+
     return render(request, 'office/main.html')
     
