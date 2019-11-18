77da925662d55dd65547a25af7d7e2bb099a0e6f
Cesar Rodriguez <merlin@Cesars-MacBook-Pro.local>
2019-11-17 21:50:32 -0500

    new login screen

--- IBKProject/views.py
+++ IBKProject/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 
 @login_required()
