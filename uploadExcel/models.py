63b76569c708efc71d1e03dedd23abccf517017b
cesar rodriguez <streamlink777@gmail.com>
2020-01-23 13:29:58 -0500

    fix model problem

--- uploadExcel/models.py
+++ uploadExcel/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 import os
+from django.utils.deconstruct import deconstructible
 
-def path_and_rename(path):
-    def wrapper(instance, filename):
+@deconstructible
+class PathRename(object):
+
+    def __init__(self, sub_path):
+        self.path = sub_path
+
+    def __call__(self, instance, filename):
         ext = filename.split('.')[-1]
-        if instance.pk:
-            filename = '{}.{}'.format(instance.pk, ext)
-        else:
-            filename = '{}.{}'.format('data', ext)
-        return os.path.join(path, filename)
-    return wrapper
+        filename = '{}.{}'.format('data', ext)
+        return os.path.join(self.path, filename)
+
+path_and_rename = PathRename("test")
 
 
 class ExcelUploadModel(models.Model):
 
     file_name = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to=path_and_rename('test/'))
+    document = models.FileField(upload_to=path_and_rename)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
